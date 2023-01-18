f12aba7290da93db2de97e51d0cefe9680fa6095
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-01-18 12:07:24 -0600

    additional shoppinglistform test

--- src/components/ShoppingList/ShoppingListForm.test.jsx
+++ src/components/ShoppingList/ShoppingListForm.test.jsx
@@ -2,7 +2,8 @@ import { screen, render, fireEvent } from '@testing-library/react';
 import ShoppingListForm from './ShoppingListForm';
 
 describe('ShoppingListForm', () => {
-  test('onSubmit, form will process data or return default', () => {
+  test('onSubmit with an empty field, default will be submitted', () => {
+
     const onSubmit = jest.fn();
     render (<ShoppingListForm onSubmit={onSubmit} id="test" />);
 
@@ -22,27 +23,12 @@ describe('ShoppingListForm', () => {
     expect(onSubmit).toHaveBeenCalledWith(defaultShoppingList);
   });
 
-  //   test('onSubmit, form will process data', () => {
-  //     const onSubmit = jest.fn();
-  //     render (<ShoppingListForm onSubmit={onSubmit} id="test" />);
+  test('typing in the input + values match', () => {
+    const onSubmit = jest.fn();
+    render (<ShoppingListForm onSubmit={onSubmit} id="test" />);
 
-  //     const newShoppingList = {
-  //       id: 1,
-  //       name: 'My New Shopping List',
-  //       shoppingItems: [],
-  //     };
-
-  //     const input = 
-  //     screen.queryByTestId('shopping-list-form-name-test');
-  //     const button = 
-  //     screen.queryByTestId('shopping-list-form-submit-button-test');
-
-//     fireEvent.change(input, { target: { newShoppingList: {
-//       id: 1,
-//       name: 'My New Shopping List',
-//       shoppingItems: [],
-//     } } });
-//     fireEvent.click(button);
-//     expect(onSubmit).toHaveBeenCalledWith(newShoppingList);
-//   });
+    const input = screen.queryByTestId('shopping-list-form-name-test');
+    fireEvent.change(input, { target: { value: 'My New List' } });
+    expect(input.value).toBe('My New List');
+  });
 });
