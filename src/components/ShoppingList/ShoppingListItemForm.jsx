d5928523a1633a3801505ed35bd54bc122c42bda
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-01-18 19:24:37 -0600

    user can add items to their specified list

--- src/components/ShoppingList/ShoppingListItemForm.jsx
+++ src/components/ShoppingList/ShoppingListItemForm.jsx
@@ -1,3 +1,35 @@
-export default function ShoppingListItemForm() {
-  return <></>;
+import { useState } from 'react';
+
+export default function ShoppingListItemForm({ onSubmit }) {
+
+  const oldShoppingItem = {
+    item: '',
+    quantity: ''
+  };
+
+  const [shoppingItem, setShoppingItem] = useState(oldShoppingItem);
+  console.log('shoppingItem', shoppingItem);
+
+  return <>
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      onSubmit(shoppingItem);
+    }}>
+      <input 
+        placeholder="Add Item" 
+        value={shoppingItem.item} 
+        onChange={(e) => setShoppingItem(
+          { ...shoppingItem, item: e.target.value }
+        )}></input>
+      <input 
+        placeholder="Quantity" 
+        value={shoppingItem.quantity} 
+        onChange={(e) => setShoppingItem(
+          { ...shoppingItem, quantity: e.target.value }
+        )}></input>
+      <button type="submit">
+    Add
+      </button>
+    </form>
+  </>;
 }
